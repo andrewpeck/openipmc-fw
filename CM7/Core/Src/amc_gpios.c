3d890b9068f115477d91463e9c8208d303e77e55
André Muller Cascadan <andre.cascadan@gmail.com>
2021-01-06 23:13:50 -0300

    Driver for expanders: get_pin_direction, set and get_pullup.

--- CM7/Core/Src/amc_gpios.c
+++ CM7/Core/Src/amc_gpios.c
@@ -1,28 +1,24 @@
 
 
-#include "main.h"
-
-#include "amc_gpios.h"
-
-struct
-{
-	uint8_t device;
-	uint8_t port; // 0 = portA; 1 = portB
-	uint8_t pin;
-
-} pin_map[90] = { {0, 0, 0}, {0, 0, 1}, {0, 0, 2}, {0, 0, 3}, {0, 0, 4}, {0, 0, 5}, {0, 0, 6}, {0, 0, 7}, {0, 1, 0}, {0, 1, 1}, // AMC0_IO_n
-                  {1, 1, 7}, {1, 1, 6}, {1, 1, 5}, {1, 1, 4}, {1, 1, 3}, {1, 1, 2}, {1, 1, 1}, {1, 1, 0}, {1, 0, 7}, {1, 0, 6}, // AMC1_IO_n
-                  {0, 1, 2}, {0, 1, 3}, {0, 1, 4}, {0, 1, 5}, {0, 1, 6}, {0, 1, 7}, {2, 0, 0}, {2, 0, 1}, {2, 0, 2}, {2, 0, 3},
-                  {1, 0, 5}, {1, 0, 4}, {1, 0, 3}, {1, 0, 2}, {1, 0, 1}, {1, 0, 0}, {3, 1, 7}, {3, 1, 6}, {3, 1, 5}, {3, 1, 4},
-                  {2, 0, 4}, {2, 0, 5}, {2, 0, 6}, {2, 0, 7}, {2, 1, 0}, {2, 1, 1}, {2, 1, 2}, {2, 1, 3}, {2, 1, 4}, {2, 1, 5},
-                  {3, 1, 3}, {3, 1, 2}, {3, 1, 1}, {3, 1, 0}, {3, 0, 7}, {3, 0, 6}, {3, 0, 5}, {3, 0, 4}, {3, 0, 3}, {3, 0, 2},
-                  {2, 1, 6}, {2, 1, 7}, {4, 0, 0}, {4, 0, 1}, {4, 0, 2}, {4, 0, 3}, {4, 0, 4}, {4, 0, 5}, {4, 0, 6}, {4, 0, 7},
-                  {3, 0, 1}, {3, 0, 0}, {5, 1, 7}, {5, 1, 6}, {5, 1, 5}, {5, 1, 4}, {5, 1, 3}, {5, 1, 2}, {5, 1, 1}, {5, 1, 0},
-                  {4, 1, 0}, {4, 1, 1}, {4, 1, 2}, {4, 1, 3}, {4, 1, 4}, {4, 1, 5}, {4, 1, 6}, {4, 1, 7}, {5, 0, 7}, {5, 0, 6}
-                                                                                                                               };
 
 /*
- * Define how to control ExPANDER_NSS and EXPANDER_RST.
+ * This file implements a driver to control all the 90 AMC_IO pins present in
+ * DIMM board.
+ *
+ * This driver takes control of all 6 MCP23S17 devices available in the
+ * board, and delivers a simple interface to control all the pins by their
+ * number.
+ *
+ */
+
+
+
+#include "main.h"
+#include "amc_gpios.h"
+
+
+/*
+ * Define how to control EXPANDER_NSS and EXPANDER_RST.
  */
 #define EXPANDER_NSS_set_low()  HAL_GPIO_WritePin( EXPANDER_NSS_GPIO_Port, EXPANDER_NSS_Pin,  GPIO_PIN_RESET);
 #define EXPANDER_NSS_set_high() HAL_GPIO_WritePin( EXPANDER_NSS_GPIO_Port, EXPANDER_NSS_Pin,  GPIO_PIN_SET  );
@@ -31,14 +27,43 @@ struct
 
 #define TIMEOUT 100
 
+
+
+/*
+ * Pin map.
+ *
+ * It tells how to control an specific AMC_IO signal in the IO expanders
+ * (MCP23S17).
+ *
+ * The array contains all AMC_IO pins organized in sequence (total of 90 pins):
+ *   AMC0_IO_0, AMC0_IO_1, AMC0_IO_2 ...
+ *                 ... AMC8_IO_7, AMC8_IO_8, AMC8_IO_9
+ */
+struct
+{
+	uint8_t device; // MCP23S17 hard wired address. From 0 to 5
+	uint8_t port;   // MCP23S17 port. 0 = PortA; 1 = PortB
+	uint8_t pin;    // MCP23S17 port pin. From 0 to 7
+
+} pin_map[90] = {
+// n = 0        1        2        3        4        5        6        7        8        9
+    {0,0,0}, {0,0,1}, {0,0,2}, {0,0,3}, {0,0,4}, {0,0,5}, {0,0,6}, {0,0,7}, {0,1,0}, {0,1,1},    // AMC0_IO_n
+    {1,1,7}, {1,1,6}, {1,1,5}, {1,1,4}, {1,1,3}, {1,1,2}, {1,1,1}, {1,1,0}, {1,0,7}, {1,0,6},    // AMC1_IO_n
+    {0,1,2}, {0,1,3}, {0,1,4}, {0,1,5}, {0,1,6}, {0,1,7}, {2,0,0}, {2,0,1}, {2,0,2}, {2,0,3},    // AMC2_IO_n
+    {1,0,5}, {1,0,4}, {1,0,3}, {1,0,2}, {1,0,1}, {1,0,0}, {3,1,7}, {3,1,6}, {3,1,5}, {3,1,4},    // AMC3_IO_n
+    {2,0,4}, {2,0,5}, {2,0,6}, {2,0,7}, {2,1,0}, {2,1,1}, {2,1,2}, {2,1,3}, {2,1,4}, {2,1,5},    // AMC4_IO_n
+    {3,1,3}, {3,1,2}, {3,1,1}, {3,1,0}, {3,0,7}, {3,0,6}, {3,0,5}, {3,0,4}, {3,0,3}, {3,0,2},    // AMC5_IO_n
+    {2,1,6}, {2,1,7}, {4,0,0}, {4,0,1}, {4,0,2}, {4,0,3}, {4,0,4}, {4,0,5}, {4,0,6}, {4,0,7},    // AMC6_IO_n
+    {3,0,1}, {3,0,0}, {5,1,7}, {5,1,6}, {5,1,5}, {5,1,4}, {5,1,3}, {5,1,2}, {5,1,1}, {5,1,0},    // AMC7_IO_n
+    {4,1,0}, {4,1,1}, {4,1,2}, {4,1,3}, {4,1,4}, {4,1,5}, {4,1,6}, {4,1,7}, {5,0,7}, {5,0,6}     // AMC8_IO_n
+};
+
+
+
+
+// SPI Handle
 extern SPI_HandleTypeDef hspi4;
 #define hspi_amc hspi4
-
-void    amc_gpios_set_expander_register(uint8_t device_addr, uint8_t reg_addr, uint8_t value);
-uint8_t amc_gpios_get_expander_register(uint8_t device_addr, uint8_t reg_addr);
-void    set_expander_register_bit(uint8_t device_addr, uint8_t reg_addr, uint8_t bit_posic, uint8_t bit_value);
-
-
 
 
 void amc_gpios_initialize_expanders( void )
@@ -145,7 +170,7 @@ uint8_t get_expander_register_bit( uint8_t device_addr, uint8_t reg_addr, uint8_
 }
 
 
-void amc_gpios_set_pin_direction( uint8_t amc_pin, amc_pin_dir_t dir )
+void amc_gpios_set_pin_direction( uint8_t amc_pin, amc_dir_t dir )
 {
 	uint8_t reg_addr;
 
@@ -156,6 +181,19 @@ void amc_gpios_set_pin_direction( uint8_t amc_pin, amc_pin_dir_t dir )
 	reg_addr = 0x00 + pin_map[amc_pin].port;
 
 	set_expander_register_bit( pin_map[amc_pin].device, reg_addr, pin_map[amc_pin].pin, (uint8_t)dir );
+}
+
+amc_dir_t amc_gpios_get_pin_direction( uint8_t amc_pin )
+{
+	uint8_t reg_addr;
+
+	// Protection against bad value
+	if(amc_pin >= 90) return 0;
+
+	// Calculate register addr IODIRA or IODIRB. Base addr: 0x00
+	reg_addr = 0x00 + pin_map[amc_pin].port;
+
+	return get_expander_register_bit( pin_map[amc_pin].device, reg_addr, pin_map[amc_pin].pin );
 }
 
 void amc_gpios_write_pin( uint8_t amc_pin, uint8_t pin_value )
@@ -180,6 +218,32 @@ uint8_t amc_gpios_read_pin( uint8_t amc_pin )
 
 	// Calculate register addr GPIOA or GPIOB. Base addr: 0x12
 	reg_addr = 0x12 + pin_map[amc_pin].port;
+
+	return get_expander_register_bit( pin_map[amc_pin].device, reg_addr, pin_map[amc_pin].pin );
+}
+
+void amc_gpios_set_pin_pullup( uint8_t amc_pin, amc_pullup_t pullup_state )
+{
+	uint8_t reg_addr;
+
+	// Protection against bad value
+	if(amc_pin >= 90) return;
+
+	// Calculate register addr GPPUA or GPPUB. Base addr: 0x0C
+	reg_addr = 0x0C + pin_map[amc_pin].port;
+
+	set_expander_register_bit( pin_map[amc_pin].device, reg_addr, pin_map[amc_pin].pin, pullup_state );
+}
+
+amc_pullup_t amc_gpios_get_pin_pullup( uint8_t amc_pin )
+{
+	uint8_t reg_addr;
+
+	// Protection against bad value
+	if(amc_pin >= 90) return 0;
+
+	// Calculate register addr GPPUA or GPPUB. Base addr: 0x0C
+	reg_addr = 0x0C + pin_map[amc_pin].port;
 
 	return get_expander_register_bit( pin_map[amc_pin].device, reg_addr, pin_map[amc_pin].pin );
 }
--- CM7/Core/Src/amc_gpios.h
+++ CM7/Core/Src/amc_gpios.h
@@ -5,14 +5,24 @@
 typedef enum
 {
     OUT = 0,
-     IN = 1
-} amc_pin_dir_t;
+     IN = 1   // Default. All pins as input after initialization.
+} amc_dir_t;
 
 
-void    amc_gpios_initialize_expanders( void );
-void    amc_gpios_set_pin_direction( uint8_t amc_pin, amc_pin_dir_t dir );
-void    amc_gpios_write_pin( uint8_t amc_pin, uint8_t pin_value );
-uint8_t amc_gpios_read_pin( uint8_t amc_pin );
+typedef enum
+{
+    OFF = 0, // Default. All pin are floating after initialization.
+     ON = 1
+} amc_pullup_t;
+
+
+void         amc_gpios_initialize_expanders( void );
+void         amc_gpios_set_pin_direction( uint8_t amc_pin, amc_dir_t dir );
+amc_dir_t    amc_gpios_get_pin_direction( uint8_t amc_pin );
+void         amc_gpios_write_pin( uint8_t amc_pin, uint8_t pin_value );
+uint8_t      amc_gpios_read_pin( uint8_t amc_pin );
+void         amc_gpios_set_pin_pullup( uint8_t amc_pin, amc_pullup_t pullup_state );
+amc_pullup_t amc_gpios_get_pin_pullup( uint8_t amc_pin );
 
 
 #endif
