382ade986ecd9922c8af4f79390821706d088197
Andrew Peck <andrew.peck@cern.ch>
2021-06-08 11:58:07 -0400

    Reformat ugliness

--- CM7/Core/Src/apollo/user_eeprom.h
+++ CM7/Core/Src/apollo/user_eeprom.h
@@ -25,49 +25,26 @@
 
 typedef unsigned char uint8_t;
 
-char
-user_eeprom_init(void);
+char user_eeprom_init(void);
+char user_eeprom_read(void);
+char user_eeprom_write(void);
 
-char
-user_eeprom_read(void);
+char user_eeprom_set_version(uint8_t v);
+char user_eeprom_get_version(uint8_t * v);
 
-char
-user_eeprom_write(void);
+char user_eeprom_get_serial_number(uint8_t * sn);
+char user_eeprom_set_serial_number(uint8_t sn);
 
-char
-user_eeprom_set_version(uint8_t v);
+char user_eeprom_get_revision_number(uint8_t * rn);
+char user_eeprom_set_revision_number(uint8_t rn);
 
-char
-user_eeprom_get_version(uint8_t * v);
+char user_eeprom_get_mac_addr(uint8_t eth, uint8_t * mac);
+char user_eeprom_set_mac_addr(uint8_t eth, uint8_t * mac);
 
-char
-user_eeprom_get_serial_number(uint8_t * sn);
+char user_eeprom_write_enable(void);
+char user_eeprom_write_disable(void);
+char user_eeprom_get_wren(uint8_t * lvl);
 
-char
-user_eeprom_set_serial_number(uint8_t sn);
-
-char
-user_eeprom_get_revision_number(uint8_t * rn);
-
-char
-user_eeprom_set_revision_number(uint8_t rn);
-
-char
-user_eeprom_get_mac_addr(uint8_t eth
-                         , uint8_t * mac);
-
-char
-user_eeprom_set_mac_addr(uint8_t eth
-                         , uint8_t * mac);
-
-char
-user_eeprom_write_enable(void);
-
-char
-user_eeprom_write_disable(void);
-
-char
-user_eeprom_get_wren(uint8_t * lvl);
 char user_eeprom_set_boot_mode(uint8_t bm);
 char user_eeprom_get_boot_mode(uint8_t *bm);
 
