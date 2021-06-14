95c7aa5e720fbec4f22a5ccaa7ac3d5bda2d82e5
Andrew Peck <andrew.peck@cern.ch>
2021-06-14 11:22:11 -0400

    Add HAL OK check to pim400 sensor

--- CM7/Core/Src/apollo/pim400.c
+++ CM7/Core/Src/apollo/pim400.c
@@ -16,18 +16,25 @@ void sensor_reading_temp_pim400 (sensor_reading_t* sensor_reading) {
   uint8_t reg_adr, rx_data;
   reg_adr = PIM400_TEMP_REG;
 
-  mgm_i2c_transmit(PIM400_I2C_ADR, &reg_adr, 1, 100 ); // (adr , data, size, timeout)
-  mgm_i2c_receive (PIM400_I2C_ADR, &rx_data, 1, 100 ); // (adr , data, size, timeout)
+  HAL_StatusTypeDef status = HAL_OK;
+  status |= mgm_i2c_transmit(PIM400_I2C_ADR, &reg_adr, 1, 100 ); // (adr , data, size, timeout)
+  status |= mgm_i2c_receive (PIM400_I2C_ADR, &rx_data, 1, 100 ); // (adr , data, size, timeout)
 
   // temperature = 1.961 C / bit  - 50C
-  sensor_reading->raw_value = rx_data;
+  if (status == HAL_OK) {
+    sensor_reading->raw_value = rx_data;
 
-  sensor_reading->present_state = 0;
+    sensor_reading->present_state = 0;
 
-  set_sensor_upper_state (sensor_reading,
-                          pim400_temp_upper_noncritical_raw,
+    set_sensor_upper_state (sensor_reading,
+                                                      pim400_temp_upper_noncritical_raw,
                           pim400_temp_upper_critical_raw,
                           pim400_temp_upper_nonrecoverable_raw);
+  } else {
+    sensor_reading->raw_value = 0;
+    sensor_reading->present_state = 0;
+  }
+
 
 }
 
@@ -35,17 +42,23 @@ void sensor_reading_iout_pim400(sensor_reading_t* sensor_reading) {
   uint8_t reg_adr, rx_data;
   reg_adr = PIM400_IOUT_REG;
 
-  mgm_i2c_transmit(PIM400_I2C_ADR, &reg_adr, 1, 100 ); // (adr , data, size, timeout)
-  mgm_i2c_receive (PIM400_I2C_ADR, &rx_data, 1, 100 ); // (adr , data, size, timeout)
+  HAL_StatusTypeDef status = HAL_OK;
+  status |= mgm_i2c_transmit(PIM400_I2C_ADR, &reg_adr, 1, 100 ); // (adr , data, size, timeout)
+  status |= mgm_i2c_receive (PIM400_I2C_ADR, &rx_data, 1, 100 ); // (adr , data, size, timeout)
 
   // current = 0.094 A / bit
-  sensor_reading->raw_value = rx_data;
-  sensor_reading->present_state = 0;
+  if (status == HAL_OK) {
+    sensor_reading->raw_value = rx_data;
+    sensor_reading->present_state = 0;
 
-  set_sensor_upper_state (sensor_reading,
-                          pim400_iout_upper_noncritical_raw,
-                          pim400_iout_upper_critical_raw,
-                          pim400_iout_upper_nonrecoverable_raw);
+    set_sensor_upper_state (sensor_reading,
+                            pim400_iout_upper_noncritical_raw,
+                            pim400_iout_upper_critical_raw,
+                            pim400_iout_upper_nonrecoverable_raw);
+  } else {
+    sensor_reading->raw_value = 0;
+    sensor_reading->present_state = 0;
+  }
 
 }
 
@@ -59,25 +72,29 @@ void sensor_reading_voltage_pim400(uint8_t supply, sensor_reading_t *sensor_read
   else
     reg_adr = PIM400_VBF_REG;
 
-  mgm_i2c_transmit(PIM400_I2C_ADR, &reg_adr, 1, 100); // (adr , data, size, timeout)
-  mgm_i2c_receive (PIM400_I2C_ADR, &rx_data, 1, 100); // (adr , data, size, timeout)
+  HAL_StatusTypeDef status = HAL_OK;
+  status |= mgm_i2c_transmit(PIM400_I2C_ADR, &reg_adr, 1, 100); // (adr , data, size, timeout)
+  status |= mgm_i2c_receive (PIM400_I2C_ADR, &rx_data, 1, 100); // (adr , data, size, timeout)
 
   // current = 0.325 V / bit
-  sensor_reading->raw_value = rx_data;
+  if (status == HAL_OK) {
+    sensor_reading->raw_value = rx_data;
 
-  sensor_reading->present_state = 0;
+    sensor_reading->present_state = 0;
 
-  set_sensor_upper_state (sensor_reading,
-                          pim400_voltage_upper_noncritical_raw,
-                          pim400_voltage_upper_critical_raw,
-                          pim400_voltage_upper_nonrecoverable_raw);
+    set_sensor_upper_state (sensor_reading,
+                            pim400_voltage_upper_noncritical_raw,
+                            pim400_voltage_upper_critical_raw,
+                            pim400_voltage_upper_nonrecoverable_raw);
 
-  set_sensor_lower_state (sensor_reading,
-                          pim400_voltage_lower_noncritical_raw,
-                          pim400_voltage_lower_critical_raw,
-                          pim400_voltage_lower_nonrecoverable_raw);
-
-
+    set_sensor_lower_state (sensor_reading,
+                            pim400_voltage_lower_noncritical_raw,
+                            pim400_voltage_lower_critical_raw,
+                            pim400_voltage_lower_nonrecoverable_raw);
+  } else {
+    sensor_reading->raw_value = 0;
+    sensor_reading->present_state = 0;
+  }
 }
 
 void sensor_reading_voltage_a_pim400(sensor_reading_t *sensor_reading) {
