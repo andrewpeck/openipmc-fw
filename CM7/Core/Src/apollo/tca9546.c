f18556d3323a82b5d63d70c01f9c422a95ded3a5
Andrew Peck <andrew.peck@cern.ch>
2021-07-14 12:23:05 -0400

    Also don't read from CM i2c if power is off

--- CM7/Core/Src/apollo/tca9546.c
+++ CM7/Core/Src/apollo/tca9546.c
@@ -1,7 +1,11 @@
 #include "sense_i2c.h"
 #include "tca9546.h"
+#include "apollo.h"
 
-HAL_StatusTypeDef tca9546_config (uint8_t mask) {
+// TODO: should keep track of the current switch state and only change if it
+// needs to
+
+HAL_StatusTypeDef tca9546_config(uint8_t mask) {
   const uint8_t tca9546_adr = 0x70 << 1;
   uint8_t tx_data = mask & 0xf;
   return sense_i2c_transmit(tca9546_adr, &tx_data, 1, 2000);
@@ -11,19 +15,26 @@ HAL_StatusTypeDef tca9546_sel_local () {
   return tca9546_config (0x1);
 }
 
-HAL_StatusTypeDef tca9546_sel_m1 () {
-  return tca9546_config (0x2);
-}
-
-HAL_StatusTypeDef tca9546_sel_m2 () {
-  return tca9546_config (0x4);
-}
-
-HAL_StatusTypeDef tca9546_sel_zynq () {
-  if (GET_12V_STATE == 1 ) {
-    return tca9546_config (0x8);
+HAL_StatusTypeDef tca9546_sel_m1() {
+  if (GET_12V_STATE == 1) {
+    return tca9546_config(0x2);
+  } else {
+    return tca9546_config(0x0);
   }
-  else  {
-    return tca9546_config (0x0);
+}
+
+HAL_StatusTypeDef tca9546_sel_m2() {
+  if (GET_12V_STATE == 1) {
+    return tca9546_config(0x4);
+  } else {
+    return tca9546_config(0x0);
+  }
+}
+
+HAL_StatusTypeDef tca9546_sel_zynq() {
+  if (GET_12V_STATE == 1) {
+    return tca9546_config(0x8);
+  } else {
+    return tca9546_config(0x0);
   }
 }
