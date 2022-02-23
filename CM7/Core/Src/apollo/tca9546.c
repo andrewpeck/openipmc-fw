44d4084fa155f7c5d4f7e675ce38c544fd791c6b
Andrew Peck <andrew.peck@cern.ch>
2022-02-23 17:15:51 -0500

    only change tca config if it needs updating
    it was accidentally changing the config in the middle of transactions,
    i.e. update tca setting, write the register address to the zynq, then
    update the tca setting again, then write the register value

--- CM7/Core/Src/apollo/tca9546.c
+++ CM7/Core/Src/apollo/tca9546.c
@@ -2,13 +2,22 @@
 #include "tca9546.h"
 #include "apollo.h"
 
+uint8_t config_last = 0x00;
+
 // TODO: should keep track of the current switch state and only change if it
 // needs to
 
 HAL_StatusTypeDef tca9546_config(uint8_t mask) {
-  const uint8_t tca9546_adr = 0x70 << 1;
-  uint8_t tx_data = mask & 0xf;
-  return sense_i2c_transmit(tca9546_adr, &tx_data, 1, 2000);
+  if (mask != config_last) {
+    config_last = mask;
+    const uint8_t tca9546_adr = 0x70 << 1;
+    uint8_t tx_data = mask & 0xf;
+    return sense_i2c_transmit(tca9546_adr, &tx_data, 1, 2000);
+  }
+  else {
+    return HAL_OK;
+  }
+
 }
 
 HAL_StatusTypeDef tca9546_sel_local () {
