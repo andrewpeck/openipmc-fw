60543727e20777fac5c981a554c74a5ab98582b3
Alp Akpinar <aakpinar@bu.edu>
2022-09-20 09:11:21 -0400

    Add ZYNQ I2C function declarations to the header file

--- apollo/zynq_i2c.h
+++ apollo/zynq_i2c.h
@@ -17,4 +17,7 @@ void zynq_set_blade_slot(uint8_t slot);
 void zynq_set_blade_rev(uint8_t rev);
 void zynq_set_ipmc_ip(uint8_t* ip);
 
+void zynq_set_eth_mac(uint8_t eth, uint8_t *mac);
+void zynq_set_eth_checksum(uint8_t eth, uint8_t checksum);
+
 #endif // ZYNQ_I2C_H_
