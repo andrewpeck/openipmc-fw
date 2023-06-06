a5fac8d3dee0317039c61be2d5a8fa288f917859
Alp Akpinar <aakpinar@bu.edu>
2023-06-06 12:26:28 -0400

    Place the sensor constants back to source file to avoid linker error

--- board_specific/atlas_temp_sensors.h
+++ board_specific/atlas_temp_sensors.h
@@ -10,22 +10,6 @@
 #define FPGA 2
 #define PM 3
 
-/* Sensor constans for the ATLAS temperature sensors */
-const linear_sensor_constants_t atlas_temp_sensor_consts =
-{
-  .sensor_type=TEMPERATURE,
-  .unit_type=DEGREES_C,
-  .lower_nonrecoverable=0,
-  .lower_noncritical=0,
-  .lower_critical=0,
-  .upper_noncritical=50,
-  .upper_critical=65,
-  .upper_nonrecoverable=80,
-  .m=1,
-  .b=0,
-  .re=0,
-  .be=0
-};
 
 sensor_reading_status_t sensor_reading_atlas(uint8_t sensor, sensor_reading_t *sensor_reading);
 
@@ -33,5 +17,7 @@ sensor_reading_status_t sensor_reading_atlas_board_temp(sensor_reading_t *sensor
 sensor_reading_status_t sensor_reading_atlas_firefly_temp(sensor_reading_t *sensor_reading);
 sensor_reading_status_t sensor_reading_atlas_fpga_temp(sensor_reading_t *sensor_reading);
 sensor_reading_status_t sensor_reading_atlas_pm_temp(sensor_reading_t *sensor_reading);
+
+extern const linear_sensor_constants_t atlas_temp_sensor_consts;
 
 #endif /* ATLAS_TEMP_SENSORS_H */
